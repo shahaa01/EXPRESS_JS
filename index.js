3939dcc78144901187250b0a8b2f3e708efee3e1
Aaditya Shah <aadishah1020@gmail.com>
2025-03-17 18:00:20 -0400

    Learning Express

---
+++ index.js
@@ -0,0 +1,15 @@
+//This will act as a server
+
+const express = require("express"); //we will require it
+const app = express(); //this returns some value which we store
+//This app variable will help us create server side application
+// console.log(app);
+
+let port = 3000; 
+/**
+ * Port - Endpoints of a network connection that is used
+ * to exchange
+ */
+
+app.listen(port, ) 
+
